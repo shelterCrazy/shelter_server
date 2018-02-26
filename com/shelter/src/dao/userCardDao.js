4886af65e31459797fff65a699d4d1ad0687fa02
kenanCrazy <253877118@qq.com>
2018-02-26 22:58:32 +0800

    获取卡牌信息接口 获取用户卡组信息接口 获取用户卡组卡牌列表信息接口

--- com/shelter/src/dao/userCardDao.js
+++ com/shelter/src/dao/userCardDao.js
@@ -12,10 +12,93 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
-//登陆查询  回掉函数方式
+/**
+ * 查询用户卡牌包信息
+ * @param userId
+ * @param fn
+ */
 exports.getUserCard = function(userId, fn){
     try {
         connection.query('select p.* from user_card_package p where p.user_id = ?', userId,
+            function (error, results, fields) {
+                if (error) throw error;
+
+                if (results != null && results.length > 0) {
+                    console.log(JSON.stringify(results))
+                    fn(true, 'Ok', results);
+                } else {
+                    console.log("查询无结果");
+                    fn(false, '查询异常', results);
+                }
+            });
+    } catch (e) {
+        console.log("查询错误");
+        fn(false, '查询异常'+e.stack);
+    }
+}
+
+
+/**
+ * 查询用户卡组信息
+ * @param userId
+ * @param fn
+ */
+exports.getUserDeck = function(userId, fn){
+    try {
+        connection.query('select d.* from user_deck d where d.user_id = ?', userId,
+            function (error, results, fields) {
+                if (error) throw error;
+
+                if (results != null && results.length > 0) {
+                    console.log(JSON.stringify(results))
+                    fn(true, 'Ok', results);
+                } else {
+                    console.log("查询无结果");
+                    fn(false, '查询异常', results);
+                }
+            });
+    } catch (e) {
+        console.log("查询错误");
+        fn(false, '查询异常'+e.stack);
+    }
+}
+
+
+/**
+ * 查询用户卡组卡牌列表
+ * @param userId
+ * @param deckId
+ * @param fn
+ */
+exports.getUserDeckCard = function(userId, deckId, fn){
+    try {
+        connection.query('select t.* from user_deck_card t where t.deck_id = ? and t.user_id = ?', [deckId, userId],
+            function (error, results, fields) {
+                if (error) throw error;
+
+                if (results != null && results.length > 0) {
+                    console.log(JSON.stringify(results))
+                    fn(true, 'Ok', results);
+                } else {
+                    console.log("查询无结果");
+                    fn(false, '查询异常', results);
+                }
+            });
+    } catch (e) {
+        console.log("查询错误");
+        fn(false, '查询异常'+e.stack);
+    }
+}
+
+
+/**
+ * 查询某张卡牌信息
+ * @param cardId
+ * @param fn
+ */
+exports.getCardInfo = function(cardId, fn){
+    try {
+        connection.query('select t.* from card_info t where t.id = ?', [cardId],
             function (error, results, fields) {
                 if (error) throw error;
 
--- com/shelter/src/userCradController.js
+++ com/shelter/src/userCradController.js
@@ -13,7 +13,7 @@ module.exports = function(appL){
 var init = function(){
 
 
-    //登陆
+    //获取用户卡牌包信息
     app.get('/areadly/userCrad', function (req, res) {
 
         res.writeHead(200, {'Content-Type': 'application/json'});
@@ -40,4 +40,99 @@ var init = function(){
         }
     });
 
+
+    /**获取用户卡组信息
+     * param: token
+     */
+    app.get('/areadly/getUserDeck', function (req, res) {
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        try{
+            var token = req.param("token");
+
+            if(token == null){
+                res.end(JSON.stringify({"status": '002', "msg": "token获取失败"}));
+            }
+            var userId = util.decode(token);
+
+            //查询用户卡组信息
+            userCard.getUserDeck(userId, function(flag, msg, results){
+                if(flag){
+                    res.write(JSON.stringify({"status": '200', "userDeckList":results}));
+                    res.end();
+                    return;
+                }else{
+                    res.end(JSON.stringify({"status": '001', "msg": "没有数据"}));
+                    return;
+                }
+            });
+        }catch (e){
+            res.end(JSON.stringify({"status": '001', "msg": e.toString()}));
+        }
+    });
+
+
+    /**
+     * 获取用户卡组卡牌信息
+     */
+    app.get('/areadly/getUserDeckCard', function (req, res) {
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        try{
+            var token = req.param("token");
+            var deckId = req.param("deckId");
+
+            if(token == null || deckId == null){
+                res.end(JSON.stringify({"status": '002', "msg": "token获取失败 或者 deckId为空"}));
+            }
+            var userId = util.decode(token);
+
+            //获取用户卡组卡牌
+            userCard.getUserDeckCard(userId, deckId, function(flag, msg, results){
+                if(flag){
+                    res.write(JSON.stringify({"status": '200', "userDeckCardList":results}));
+                    res.end();
+                    return;
+                }else{
+                    res.end(JSON.stringify({"status": '001', "msg": "没有数据"}));
+                    return;
+                }
+            });
+        }catch (e){
+            res.end(JSON.stringify({"status": '001', "msg": e.toString()}));
+        }
+    });
+
+
+
+    /**
+     * 获取卡牌信息
+     */
+    app.get('/areadly/getCardInfo', function (req, res) {
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        try{
+            var token = req.param("token");
+            var cardId = req.param("cardId");
+
+            if(token == null || cardId == null){
+                res.end(JSON.stringify({"status": '002', "msg": "token获取失败 或者 deckId为空"}));
+            }
+            var userId = util.decode(token);
+
+            //获取用户卡组卡牌
+            userCard.getCardInfo(cardId, function(flag, msg, results){
+                if(flag){
+                    res.write(JSON.stringify({"status": '200', "cardInfo":results}));
+                    res.end();
+                    return;
+                }else{
+                    res.end(JSON.stringify({"status": '001', "msg": "没有数据"}));
+                    return;
+                }
+            });
+        }catch (e){
+            res.end(JSON.stringify({"status": '001', "msg": e.toString()}));
+        }
+    });
 }
